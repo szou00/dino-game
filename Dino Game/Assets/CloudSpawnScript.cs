5937b2bf6db070533e9466e1251e38a6ece0022b
fionamei131@gmail.com <fionamei131@gmail.com>
2024-01-21 16:31:36 -0500

    made clouds

---
+++ Dino Game/Assets/CloudSpawnScript.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudSpawnScript : MonoBehaviour
+{
+    public GameObject cloud;
+    private float spawnRate = 8;
+    private float timer = 0;
+    public float heightOffset = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnCloud();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer < spawnRate)
+        {
+            timer += Time.deltaTime;
+        }
+        else
+        {
+            spawnCloud();
+            timer = 0;
+        }
+    }
+
+    void spawnCloud()
+    {
+        float lowestPoint = transform.position.y - heightOffset;
+        float highestPoint = transform.position.y + heightOffset;
+
+        Instantiate(cloud, new Vector3(transform.position.x, Random.Range(lowestPoint, highestPoint), 0), transform.rotation);
+    }
+}
